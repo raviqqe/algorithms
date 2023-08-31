8351f806bdb27654548febd26eb7903d063d4494
Yota Toyama <raviqqe@gmail.com>
2023-08-31 20:41:29 +1000

    Fix

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -2,20 +2,25 @@ pub fn binary_search<T: Ord>(xs: &[T], y: &T) -> Result<usize, usize> {
     let mut index = xs.len() / 2;
     let mut step = xs.len();
 
-    while step > 0 {
+    while index < xs.len() {
         step = step / 2;
 
         let x = &xs[index];
 
         if x == y {
             return Ok(index);
-        } else if x < y {
+        }
+
+        let less = x < y;
+
+        if less {
             index += step;
-            if step == 0 {
-                index += 1;
-            }
-        } else if x > y {
+        } else {
             index -= step;
+        }
+
+        if step == 0 {
+            return Err(index + if less { 1 } else { 0 });
         }
     }
 
@@ -51,7 +56,10 @@ mod tests {
     fn search_3_elements() {
         assert_eq!(binary_search(&[1, 3, 5], &0), Err(0));
         assert_eq!(binary_search(&[1, 3, 5], &1), Ok(0));
+        assert_eq!(binary_search(&[1, 3, 5], &2), Err(1));
         assert_eq!(binary_search(&[1, 3, 5], &3), Ok(1));
+        assert_eq!(binary_search(&[1, 3, 5], &4), Err(2));
         assert_eq!(binary_search(&[1, 3, 5], &5), Ok(2));
+        assert_eq!(binary_search(&[1, 3, 5], &6), Err(3));
     }
 }
