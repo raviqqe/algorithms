8c55584ead790c87acee6c05bce8c8458629c6ae
Yota Toyama <raviqqe@gmail.com>
2023-09-11 20:53:37 +1000

    Fix

--- src/leet_code/p6.rs
+++ src/leet_code/p6.rs
@@ -1,11 +1,15 @@
 pub fn convert(s: String, row_count: i32) -> String {
     let row_count = row_count as usize;
-    let a = 2 * (row_count - 1);
+    let a = if row_count > 1 {
+        2 * (row_count - 1)
+    } else {
+        1
+    };
     let xs = s.as_bytes();
     let mut ys = Vec::<u8>::with_capacity(s.len());
 
     for i in 0..row_count {
-        for j in 0..xs.len() / a.max(1) + 1 {
+        for j in 0..xs.len() / a + 1 {
             let k = i + j * a;
 
             ys.extend(xs.get(k).copied());
@@ -27,8 +31,10 @@ mod tests {
     #[test]
     fn test() {
         assert_eq!(convert("".into(), 1), "".to_string());
+        assert_eq!(convert("a".into(), 1), "a".to_string());
+        assert_eq!(convert("ab".into(), 1), "ab".to_string());
         assert_eq!(convert("foo".into(), 3), "foo".to_string());
-        assert_eq!(convert("PAYPALISHIRING".into(), 3), "PAHNAPLSIIGYIR");
-        assert_eq!(convert("PAYPALISHIRING".into(), 4), "PINALSIGYAHRPI");
+        assert_eq!(convert("paypalishiring".into(), 3), "pahnaplsiigyir");
+        assert_eq!(convert("paypalishiring".into(), 4), "pinalsigyahrpi");
     }
 }
