00524b59a3dc42d811956804988c33a15c5f8962
Yota Toyama <raviqqe@gmail.com>
2023-09-11 16:26:53 +1000

    Fix

--- src/leet_code.rs
+++ src/leet_code.rs
@@ -1,5 +1,6 @@
-pub mod p118_pascal_triangle;
-pub mod p1_two_sum;
-pub mod p2551_marble_bag;
-pub mod p2_add_two_numbers;
-pub mod p3_longest_substring_without_repeating_characters;
+pub mod p1;
+pub mod p118;
+pub mod p2;
+pub mod p2551;
+pub mod p3;
+pub mod p4;
---
+++ src/leet_code/p4.rs
@@ -0,0 +1,3 @@
+pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    0.0
+}
