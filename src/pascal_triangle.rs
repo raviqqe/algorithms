ef2d5e597a7c281e9634fcf82b110e871b79738c
Yota Toyama <raviqqe@gmail.com>
2023-09-11 10:31:41 +1000

    Fix

--- src/pascal_triangle.rs
+++ src/pascal_triangle.rs
@@ -5,18 +5,18 @@ pub fn pascal_triangle(row_count: i32) -> Vec<Vec<i32>> {
     let mut xs = vec![] as Vec<Vec<i32>>;
 
     for i in 0..row_count as usize {
-        xs.push(
-            (0..i + 1)
-                .map(|j| {
-                    if let Some(xs) = xs.get(i.saturating_sub(1)) {
-                        (if j == 0 { 0 } else { xs[j - 1] })
-                            + xs.get(j).copied().unwrap_or_default()
-                    } else {
-                        1
-                    }
-                })
-                .collect(),
-        );
+        let len = i + 1;
+        let mut ys = Vec::with_capacity(len);
+
+        for j in 0..len {
+            ys.push(if let Some(xs) = xs.get(i.saturating_sub(1)) {
+                (if j == 0 { 0 } else { xs[j - 1] }) + xs.get(j).copied().unwrap_or_default()
+            } else {
+                1
+            });
+        }
+
+        xs.push(ys);
     }
 
     xs
