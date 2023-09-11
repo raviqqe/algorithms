006e664841aa1aec2ad741f234129445dbb94613
Yota Toyama <raviqqe@gmail.com>
2023-09-11 21:19:47 +1000

    Fix

--- src/leet_code/p7.rs
+++ src/leet_code/p7.rs
@@ -1,6 +1,12 @@
-pub fn reverse(x: i32) -> i32 {
-    while x != 0 {
-        x
+pub fn reverse(mut x: i32) -> i32 {
+    let mut y = 0;
+
+    while {
+        y *= 10;
+        x != 0
+    } {
+        y += x % 10;
+        x /= 10;
     }
 
     0
@@ -15,11 +21,9 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(convert("".into(), 1), "".to_string());
-        assert_eq!(convert("a".into(), 1), "a".to_string());
-        assert_eq!(convert("ab".into(), 1), "ab".to_string());
-        assert_eq!(convert("foo".into(), 3), "foo".to_string());
-        assert_eq!(convert("paypalishiring".into(), 3), "pahnaplsiigyir");
-        assert_eq!(convert("paypalishiring".into(), 4), "pinalsigyahrpi");
+        assert_eq!(reverse(0), 0);
+        assert_eq!(reverse(1), 1);
+        assert_eq!(reverse(2), 2);
+        assert_eq!(reverse(-42), -42);
     }
 }
