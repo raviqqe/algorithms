a25b944a512d53145ae12d1356e807610190bb3d
Yota Toyama <raviqqe@gmail.com>
2023-09-11 20:05:50 +1000

    Fix

--- src/leet_code/p6.rs
+++ src/leet_code/p6.rs
@@ -1,3 +1,28 @@
 pub fn convert(s: String, row_count: i32) -> String {
-    "".into()
+    let mut ys = Vec::<u8>::with_capacity(s.len());
+
+    for i in 0..row_count {
+        ys.extend(
+            if i == 0 || i == row_count - 1 {
+                ys.get(i)
+            } else {
+                ys.get(i)
+            }
+            .copied(),
+        );
+    }
+
+    String::from_utf8(ys).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test() {
+        assert_eq!(convert("".into(), 0), "".to_string());
+        assert_eq!(convert("foo".into(), 3), "foo".to_string());
+    }
 }
