d7ed56fd89b540bbe9bba0e3ecae7c01114b70dc
Yota Toyama <raviqqe@gmail.com>
2023-09-11 16:36:22 +1000

    Fix

--- src/leet_code/p4.rs
+++ src/leet_code/p4.rs
@@ -1,3 +1,10 @@
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    0.0
+    let i = nums1.len() / 2;
+    let j = nums2.len() / 2;
+
+    while i + j == (nums1.len() + nums2.len()) / 2 {
+        todo!();
+    }
+
+    nums1[i] as f64 + nums2[j] as f64
 }
