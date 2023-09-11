c2212a77d1eab8c2176f4a889489fe0f2b5e0e02
Yota Toyama <raviqqe@gmail.com>
2023-09-11 22:22:52 +1000

    Fix

--- src/leet_code/p8.rs
+++ src/leet_code/p8.rs
@@ -3,12 +3,15 @@ pub fn atoi(s: String) -> i32 {
     let mut i = 0;
     let mut y = 0i32;
 
-    while !is_digit(xs[i]) && ![b'+', b'-'].contains(&xs[i]) {
+    while xs[i] == b' ' {
         i += 1;
     }
 
+    if !is_digit(xs[i]) && !b"+-".contains(&xs[i]) {
+        return 0;
+    }
+
     let s = if xs[i] == b'-' { -1 } else { 1 };
-    dbg!(s);
 
     while !is_digit(xs[i]) {
         i += 1;
@@ -55,5 +58,10 @@ mod tests {
     fn space() {
         assert_eq!(atoi(" 1".into()), 1);
         assert_eq!(atoi("  1".into()), 1);
+    }
+
+    #[test]
+    fn invalid() {
+        assert_eq!(atoi("words and 987".into()), 0);
     }
 }
