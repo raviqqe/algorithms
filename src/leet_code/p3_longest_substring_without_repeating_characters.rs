558fbdb2e972d0d02fa8a149957407cf4f94fb57
Yota Toyama <raviqqe@gmail.com>
2023-09-11 16:21:37 +1000

    Fix

--- src/leet_code/p3_longest_substring_without_repeating_characters.rs
+++ src/leet_code/p3_longest_substring_without_repeating_characters.rs
@@ -34,9 +34,9 @@ mod tests {
         assert_eq!(length_of_longest_substring("".into()), 0);
         assert_eq!(length_of_longest_substring(" ".into()), 1);
         assert_eq!(length_of_longest_substring("abcabcbb".into()), 3);
-        // assert_eq!(length_of_longest_substring("bbbbb".into()), 1);
-        // assert_eq!(length_of_longest_substring("abcabcdbb".into()), 4);
-        // assert_eq!(length_of_longest_substring("pwwkew".into()), 3);
-        // assert_eq!(length_of_longest_substring("dvdf".into()), 3);
+        assert_eq!(length_of_longest_substring("bbbbb".into()), 1);
+        assert_eq!(length_of_longest_substring("abcabcdbb".into()), 4);
+        assert_eq!(length_of_longest_substring("pwwkew".into()), 3);
+        assert_eq!(length_of_longest_substring("dvdf".into()), 3);
     }
 }
