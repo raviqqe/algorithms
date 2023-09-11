f9257d56923e224f9c3e12d6c8cb219b55968aaf
Yota Toyama <raviqqe@gmail.com>
2023-09-11 23:58:52 +1000

    Fix

--- src/leet_code/p10.rs
+++ src/leet_code/p10.rs
@@ -1,26 +1,27 @@
 pub fn is_match(s: String, p: String) -> bool {
-    let mut xs = s.chars();
-    let mut ys = p.chars();
-    let mut y = ys.next();
+    let xs = s.as_bytes();
+    let ys = p.as_bytes();
+    let mut i = 0;
+    let mut j = 0;
 
-    while y.is_some() {
-        let z = ys.next();
-
-        match z {
-            Some('*') => todo!(),
-            Some(_) | None => {
-                let x = xs.next();
-
-                if y != Some('.') && x != y {
-                    return false;
-                }
+    while j < ys.len() {
+        if ys[j] == b'*' {
+        } else if ys.get(j + 1) == Some(&b'*') {
+            while i < xs.len() && (ys[j] == b'.' || xs[i] == ys[j]) {
+                i += 1;
             }
+        } else {
+            if ys[j] != b'.' && xs.get(i) != Some(&ys[j]) {
+                return false;
+            }
+
+            i += 1;
         }
 
-        y = z;
+        j += 1;
     }
 
-    xs.next().is_none()
+    i == xs.len() && j == ys.len()
 }
 
 #[cfg(test)]
@@ -34,5 +35,23 @@ mod tests {
         assert_eq!(is_match("a".into(), "".into()), false);
         assert_eq!(is_match("".into(), "a".into()), false);
         assert_eq!(is_match("a".into(), "a".into()), true);
+        assert_eq!(is_match("ab".into(), "a".into()), false);
+        assert_eq!(is_match("a".into(), "ab".into()), false);
+        assert_eq!(is_match("ab".into(), "ab".into()), true);
+    }
+
+    #[test]
+    fn dot() {
+        assert_eq!(is_match("a".into(), ".".into()), true);
+        assert_eq!(is_match("ab".into(), "..".into()), true);
+    }
+
+    #[test]
+    fn asterisk() {
+        assert_eq!(is_match("".into(), "a*".into()), true);
+        assert_eq!(is_match("a".into(), "a*".into()), true);
+        assert_eq!(is_match("aa".into(), "a*".into()), true);
+        assert_eq!(is_match("aaa".into(), "a*".into()), true);
+        assert_eq!(is_match("ab".into(), "..".into()), true);
     }
 }
