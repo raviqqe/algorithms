908c98d2d10023e621b30e45c2fa0ff2708158e2
Yota Toyama <raviqqe@gmail.com>
2023-09-11 19:00:05 +1000

    Fix

--- src/leet_code/p5.rs
+++ src/leet_code/p5.rs
@@ -1,5 +1,32 @@
 pub fn longest_palindrome(s: String) -> String {
-    s
+    let xs = s.as_bytes();
+    let mut s = b"" as &[u8];
+
+    for i in 0..xs.len() {
+        for j in i..xs.len() {
+            let ys = &xs[i..j + 1];
+
+            for k in 0..2 {
+                let j = j + k;
+
+                if j + ys.len() > xs.len() {
+                    continue;
+                }
+
+                let zs = &xs[j..j + ys.len()];
+
+                if ys.len() == zs.len() && ys.iter().zip(zs.iter().rev()).all(|(y, z)| y == z) {
+                    let vs = &xs[i..j + ys.len()];
+
+                    if vs.len() > s.len() {
+                        s = vs;
+                    }
+                }
+            }
+        }
+    }
+
+    String::from_utf8(s.to_vec()).unwrap()
 }
 
 #[cfg(test)]
@@ -8,7 +35,26 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn simple() {
+    fn zero() {
         assert_eq!(longest_palindrome("".into()), "".to_string());
+    }
+
+    #[test]
+    fn one() {
+        assert_eq!(longest_palindrome("a".into()), "a".to_string());
+        assert_eq!(longest_palindrome("ab".into()), "a".to_string());
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(longest_palindrome("aa".into()), "aa".to_string());
+        assert_eq!(longest_palindrome("aab".into()), "aa".to_string());
+        assert_eq!(longest_palindrome("baa".into()), "aa".to_string());
+    }
+
+    #[test]
+    fn more() {
+        assert_eq!(longest_palindrome("abcba".into()), "abcba".to_string());
+        assert_eq!(longest_palindrome("xabcbay".into()), "abcba".to_string());
     }
 }
