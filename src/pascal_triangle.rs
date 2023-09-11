46e1cf4a3893420329fe1407540fd3107848d479
Yota Toyama <raviqqe@gmail.com>
2023-09-11 10:22:17 +1000

    Fix

--- src/pascal_triangle.rs
+++ src/pascal_triangle.rs
@@ -8,8 +8,9 @@ pub fn pascal_triangle(row_count: i32) -> Vec<Vec<i32>> {
         xs.push(
             (0..i + 1)
                 .map(|j| {
-                    if let Some(xs) = xs.get(i - 1) {
-                        xs[j] + xs.get(j + 1).copied().unwrap_or_default()
+                    if let Some(xs) = xs.get(i.saturating_sub(1)) {
+                        (if j == 0 { 0 } else { xs[j - 1] })
+                            + xs.get(j).copied().unwrap_or_default()
                     } else {
                         1
                     }
