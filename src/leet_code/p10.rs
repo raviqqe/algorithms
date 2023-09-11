36069040515becc9fd0874f81de96b3ebe3f883b
Yota Toyama <raviqqe@gmail.com>
2023-09-11 23:38:40 +1000

    Fix

--- src/leet_code/p10.rs
+++ src/leet_code/p10.rs
@@ -1,3 +1,38 @@
 pub fn is_match(s: String, p: String) -> bool {
-    false
+    let mut xs = s.chars();
+    let mut ys = p.chars();
+    let mut y = ys.next();
+
+    while y.is_some() {
+        let z = ys.next();
+
+        match z {
+            Some('*') => todo!(),
+            Some(_) | None => {
+                let x = xs.next();
+
+                if y != Some('.') && x != y {
+                    return false;
+                }
+            }
+        }
+
+        y = z;
+    }
+
+    xs.count() == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple() {
+        assert_eq!(is_match("".into(), "".into()), true);
+        assert_eq!(is_match("a".into(), "".into()), false);
+        assert_eq!(is_match("".into(), "a".into()), false);
+        assert_eq!(is_match("a".into(), "a".into()), true);
+    }
 }
