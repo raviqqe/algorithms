398b0b59fcf4d0ba6235d0944492e5fcc1237d45
Yota Toyama <raviqqe@gmail.com>
2023-09-11 15:34:53 +1000

    Fix

--- src/leet_code/p3_longest_substring_without_repeating_characters.rs
+++ src/leet_code/p3_longest_substring_without_repeating_characters.rs
@@ -1,3 +1,34 @@
+use std::collections::HashSet;
+
 pub fn length_of_longest_substring(s: String) -> i32 {
-    0
+    let mut max = 0;
+    let mut cs = HashSet::<char>::new();
+    let mut len = 0;
+
+    for c in s.chars() {
+        if cs.contains(&c) {
+            max = max.max(len);
+            cs = [c].into_iter().collect();
+            len = 1;
+        } else {
+            len += 1;
+            cs.insert(c);
+        }
+    }
+
+    max
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple() {
+        assert_eq!(length_of_longest_substring("abcabcbb".into()), 3);
+        assert_eq!(length_of_longest_substring("bbbbb".into()), 1);
+        assert_eq!(length_of_longest_substring("abcabcdbb".into()), 4);
+        assert_eq!(length_of_longest_substring("pwwkew".into()), 3);
+    }
 }
