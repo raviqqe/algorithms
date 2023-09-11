eb653a4bd5a29080615f5181dcc104127bfa0783
Yota Toyama <raviqqe@gmail.com>
2023-09-11 21:14:58 +1000

    Fix

---
+++ src/leet_code/p7.rs
@@ -0,0 +1,19 @@
+pub fn reverse(x: i32) -> i32 {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    // spell-checker: disable
+
+    #[test]
+    fn test() {
+        assert_eq!(convert("".into(), 1), "".to_string());
+        assert_eq!(convert("a".into(), 1), "a".to_string());
+        assert_eq!(convert("ab".into(), 1), "ab".to_string());
+        assert_eq!(convert("foo".into(), 3), "foo".to_string());
+        assert_eq!(convert("paypalishiring".into(), 3), "pahnaplsiigyir");
+        assert_eq!(convert("paypalishiring".into(), 4), "pinalsigyahrpi");
+    }
+}
