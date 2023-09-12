4e1c67ddf03dbb4465d26d3ce39e7bbeac4c0cd7
Yota Toyama <raviqqe@gmail.com>
2023-09-12 15:13:19 +1000

    Fix

--- src/tessoku/a61.rs
+++ src/tessoku/a61.rs
@@ -3,6 +3,8 @@ use std::io::{stdin, Read};
 pub fn run() {
     let mut s = String::new();
     stdin().read_to_string(&mut s).unwrap();
-
-    s.split('\n');
+    let xs = s
+        .split('\n')
+        .map(|s| s.split(' ').collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 }
