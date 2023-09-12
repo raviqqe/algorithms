4af551d53e2f42c32f83b09e88354189bf4b2e65
Yota Toyama <raviqqe@gmail.com>
2023-09-13 09:54:10 +1000

    Fix

--- src/bin/a66.rs
+++ src/bin/a66.rs
@@ -17,15 +17,44 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let xs = &xs[1][..];
+    let [n, _] = &xs[0][..] else { return };
 
-    let mut ys = vec![0; xs.len() + 1];
+    let mut ys = vec![None; *n];
 
-    for (i, x) in xs.iter().enumerate().rev() {
-        ys[x - 1] += ys[i + 1] + 1;
+    for xs in &xs[1..] {
+        let q = xs[0];
+        let i = xs[1] - 1;
+        let j = xs[2] - 1;
+
+        if q == 1 {
+            union(i, j, &mut ys);
+        } else {
+            println!(
+                "{} ",
+                if root(i, &ys) == root(j, &ys) {
+                    "Yes"
+                } else {
+                    "No"
+                }
+            );
+        }
     }
+}
 
-    for y in ys {
-        print!("{} ", y);
+fn union(x: usize, y: usize, zs: &mut [Option<usize>]) {
+    match (zs[x], zs[y]) {
+        (None, None) | (None, Some(_)) => zs[x] = Some(y),
+        (Some(_), None) => union(y, x, zs),
+        (Some(_), Some(_)) => zs[root(x, zs)] = Some(root(y, zs)),
+    }
+}
+
+fn root(x: usize, ys: &[Option<usize>]) -> usize {
+    let y = ys[x];
+
+    if let Some(y) = y {
+        root(y, ys)
+    } else {
+        x
     }
 }
