47f5afcf84ae7ee5e1ad933bdb46fe15753c37e4
Yota Toyama <raviqqe@gmail.com>
2023-09-12 15:38:28 +1000

    Fix

---
+++ src/bin/a62.rs
@@ -0,0 +1,38 @@
+use std::{
+    collections::HashSet,
+    io::{stdin, Read},
+};
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+
+    let xs = s
+        .split('\n')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            s.split(' ')
+                .map(|s| s.trim())
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<_>>();
+
+    let [n, _] = &xs[0][..] else { return };
+
+    let mut ys = vec![HashSet::new(); *n];
+
+    for zs in &xs[1..] {
+        let i = zs[0];
+        let j = zs[1];
+
+        ys[i - 1].insert(j);
+        ys[j - 1].insert(i);
+    }
+
+    for (i, ys) in ys.iter().enumerate() {
+        println!("{}: {:?}", i + 1, ys);
+    }
+}
