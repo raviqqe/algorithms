d31bdebbc0a78e318435d979721102fd7bb1a2e1
Yota Toyama <raviqqe@gmail.com>
2023-09-12 19:33:30 +1000

    Fix

---
+++ src/bin/a65.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut s = String::new();
+    stdin().read_to_string(&mut s).unwrap();
+
+    let xs = s
+        .split('\n')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            s.split(' ')
+                .map(|s| s.trim())
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<_>>();
+
+    let xs = &xs[1][..];
+
+    let mut ys = vec![0; xs.len() + 1];
+
+    for (i, x) in xs.iter().enumerate().rev() {
+        ys[x - 1] += ys[i + 1] + 1;
+    }
+
+    for y in ys {
+        print!("{} ", y);
+    }
+}
