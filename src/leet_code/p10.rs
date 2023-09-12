43bd147a562e096ccc4271ce28099177335675b1
Yota Toyama <raviqqe@gmail.com>
2023-09-12 13:14:12 +1000

    Fix

--- src/leet_code/p10.rs
+++ src/leet_code/p10.rs
@@ -3,26 +3,29 @@ pub fn is_match(s: String, p: String) -> bool {
     let xs = s.as_bytes();
     let ys = p.as_bytes();
 
-    let mut zs = vec![vec![false; xs.len() + 1]; ys.len() + 1];
+    let mut zs = vec![vec![false; ys.len() + 1]; xs.len() + 1];
 
-    zs[0][0] = true;
+    for i in 0..xs.len() + 1 {
+        for j in 0..ys.len() + 1 {
+            let ii = i.wrapping_sub(1);
+            let jj = j.wrapping_sub(1);
+            let z = zs.get(ii).and_then(|zs| zs.get(jj));
 
-    for i in 0..ys.len() {
-        for j in 0..zs[i].len() {
-            if !zs[i][j] {
-                continue;
-            }
-
-            dbg!(i, j, ys[i], xs.get(j));
-            match (ys[i], xs.get(j)) {
-                (b'.', Some(_)) => zs[i + 1][j + 1] = true,
-                (y, Some(&x)) => zs[i + 1][j + 1] = x == y,
-                (_, None) => {}
-            }
+            zs[i][j] = match (xs.get(ii), ys.get(jj), z) {
+                (None, None, _) => true,
+                (None, Some(b'*'), _) => zs[i][j - 2],
+                (Some(&x), Some(b'*'), _) => zs[i][j - 2] || x == ys[j - 2],
+                (None, Some(_), _) => false,
+                (Some(_), None, _) => false,
+                (Some(_), Some(b'.'), Some(&z)) => z && true,
+                (Some(x), Some(y), Some(&z)) => z && x == y,
+                (Some(_), Some(_), None) => unreachable!(),
+            };
         }
     }
+    dbg!(&zs);
 
-    zs[ys.len()][xs.len()]
+    zs[xs.len()][ys.len()]
 }
 
 #[cfg(test)]
@@ -50,15 +53,17 @@ mod tests {
 
     #[test]
     fn asterisk() {
-        // assert_eq!(is_match("".into(), "a*".into()), true);
-        // assert_eq!(is_match("a".into(), "a*".into()), true);
-        // assert_eq!(is_match("aa".into(), "a*".into()), true);
-        // assert_eq!(is_match("aaa".into(), "a*".into()), true);
-        // assert_eq!(is_match("ab".into(), "..".into()), true);
+        assert_eq!(is_match("abbc".into(), "ab*c".into()), true);
+        assert_eq!(is_match("".into(), "a*".into()), true);
+        assert_eq!(is_match("a".into(), "a*".into()), true);
+        assert_eq!(is_match("aa".into(), "a*".into()), true);
+        assert_eq!(is_match("aaa".into(), "a*".into()), true);
+        assert_eq!(is_match("ab".into(), "..".into()), true);
+        assert_eq!(is_match("".into(), "aa*".into()), false);
     }
 
     #[test]
     fn non_exhaustive_asterisk() {
-        // assert_eq!(is_match("aaa".into(), "a*a".into()), true);
+        assert_eq!(is_match("aaa".into(), "a*a".into()), true);
     }
 }
