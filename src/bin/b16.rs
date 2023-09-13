ee0a1c5d146041b4f7fa3a5bf4e1776bbe050702
Yota Toyama <raviqqe@gmail.com>
2023-09-13 13:13:20 +1000

    Fix

--- src/bin/b16.rs
+++ src/bin/b16.rs
@@ -3,17 +3,16 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        ls: [usize; n - 1],
-        ms: [usize; n - 2],
+        hs: [usize; n],
     }
 
     let mut xs = vec![0; n];
 
-    for i in 1..xs.len() {
-        xs[i] = (xs[i - 1] + ls[i - 1]).min(if i == 1 {
+    for i in 0..xs.len() {
+        xs[i] = (if i == 0 { 0 } else { xs[i - 1] } + ls[i]).min(if i == 0 {
             usize::MAX
         } else {
-            xs[i - 2] + ms[i - 2]
+            (if i == 1 { 0 } else { xs[i - 2] }) + ms[i - 1]
         });
     }
 
