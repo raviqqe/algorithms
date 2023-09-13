bed3b85f3d067ba8e0b143630ce941d95db85ef2
Yota Toyama <raviqqe@gmail.com>
2023-09-13 18:17:44 +1000

    Fix

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -1,7 +1,5 @@
 use proconio::input;
 
-const V: usize = 1000;
-
 fn main() {
     input! {
         n: usize,
@@ -9,45 +7,20 @@ fn main() {
         xs: [(usize, usize); n],
     }
 
-    let m = V * n;
-    let mut dp = vec![vec![None; m + 1]; n + 1];
+    let mut dp = vec![vec![None; w + 1]; n + 1];
     dp[0][0] = Some(0);
 
     for i in 1..=n {
-        for j in 0..m + 1 {
+        for j in 0..=w {
             let (w, v) = xs[i - 1];
 
-            dp[i][j] = match (
-                dp[i - 1][j],
-                if j >= v {
-                    dp[i - 1][j - v].map(|x| x + w)
-                } else {
-                    None
-                },
-            ) {
-                (Some(w1), Some(w2)) => Some(w1.min(w2)),
-                (Some(w), None) | (None, Some(w)) => Some(w),
-                (None, None) => None,
-            };
+            dp[i][j] = dp[i - 1][j].max(if j >= w {
+                dp[i - 1][j - w].map(|x| x + v)
+            } else {
+                None
+            });
         }
     }
 
-    println!(
-        "{}",
-        dp.last()
-            .unwrap()
-            .iter()
-            .enumerate()
-            .rev()
-            .find_map(|(i, x)| if let Some(x) = x {
-                if *x <= w {
-                    Some(i)
-                } else {
-                    None
-                }
-            } else {
-                None
-            })
-            .unwrap()
-    );
+    println!("{}", dp.iter().flatten().flatten().max().unwrap());
 }
