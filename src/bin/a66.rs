e88b6c451531519c0733530c88eca73af02942e1
Yota Toyama <raviqqe@gmail.com>
2023-09-13 10:38:16 +0930

    Tessoku (#17)

---
+++ src/bin/a66.rs
@@ -0,0 +1,50 @@
+use proconio::{input, marker::Usize1};
+use std::mem::swap;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        qs: [(Usize1, Usize1, Usize1); m],
+    }
+
+    let mut zs = vec![None; n];
+    let mut ns = vec![1; n];
+
+    for (q, i, j) in qs {
+        if q == 0 {
+            union(i, j, &mut zs, &mut ns);
+        } else {
+            println!(
+                "{} ",
+                if root(i, &zs) == root(j, &zs) {
+                    "Yes"
+                } else {
+                    "No"
+                }
+            );
+        }
+    }
+}
+
+fn union(x: usize, y: usize, zs: &mut [Option<usize>], ns: &mut [usize]) {
+    let mut x = root(x, zs);
+    let mut y = root(y, zs);
+
+    if x == y {
+        return;
+    } else if ns[x] > ns[y] {
+        swap(&mut x, &mut y);
+    }
+
+    zs[x] = Some(y);
+    ns[y] += ns[x];
+}
+
+fn root(mut x: usize, ys: &[Option<usize>]) -> usize {
+    while let Some(y) = ys[x] {
+        x = y;
+    }
+
+    x
+}
