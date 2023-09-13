9b675d11ebb88171f28c0f1d06a6540cc0474f5b
Yota Toyama <raviqqe@gmail.com>
2023-09-13 14:04:05 +1000

    Add b17

---
+++ src/bin/b17.rs
@@ -0,0 +1,39 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        hs: [isize; n],
+    }
+
+    let mut xs = vec![0; n];
+
+    let cost = |i, j| ((hs[i] - hs[j]) as isize).abs() as usize;
+
+    for i in 1..xs.len() {
+        xs[i] = (xs[i - 1] + cost(i - 1, i)).min(if i == 1 {
+            usize::MAX
+        } else {
+            xs[i - 2] + cost(i - 2, i)
+        });
+    }
+
+    let mut ys = vec![xs.len() - 1];
+
+    while *ys.last().unwrap() > 0 {
+        let i = *ys.last().unwrap();
+        ys.push(
+            i - if xs[i] == xs[i - 1] + cost(i - 1, i) {
+                1
+            } else {
+                2
+            },
+        )
+    }
+
+    println!("{}", ys.len());
+
+    for y in ys.iter().rev() {
+        print!("{} ", y + 1);
+    }
+}
