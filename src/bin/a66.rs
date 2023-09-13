cd807bfe75244d5c96ab3323bb64b346c4755d2d
Yota Toyama <raviqqe@gmail.com>
2023-09-13 11:01:43 +1000

    Fix

--- src/bin/a66.rs
+++ src/bin/a66.rs
@@ -1,36 +1,18 @@
-use std::{
-    io::{stdin, Read},
-    mem::swap,
-};
+use proconio::{input, marker::Usize1};
+use std::mem::swap;
 
 fn main() {
-    let mut s = String::new();
-    stdin().read_to_string(&mut s).unwrap();
+    input! {
+        n: usize,
+        m: usize,
+        qs: [(Usize1, Usize1, Usize1); m],
+    }
 
-    let xs = s
-        .split('\n')
-        .map(|s| s.trim())
-        .filter(|s| !s.is_empty())
-        .map(|s| {
-            s.split(' ')
-                .map(|s| s.trim())
-                .filter(|s| !s.is_empty())
-                .map(|s| s.parse().unwrap())
-                .collect::<Vec<usize>>()
-        })
-        .collect::<Vec<_>>();
+    let mut zs = vec![None; n];
+    let mut ns = vec![1; n];
 
-    let [n, _] = &xs[0][..] else { return };
-
-    let mut zs = vec![None; *n];
-    let mut ns = vec![1; *n];
-
-    for xs in &xs[1..] {
-        let q = xs[0];
-        let i = xs[1] - 1;
-        let j = xs[2] - 1;
-
-        if q == 1 {
+    for (q, i, j) in qs {
+        if q == 0 {
             union(i, j, &mut zs, &mut ns);
         } else {
             println!(
@@ -45,18 +27,18 @@ fn main() {
     }
 }
 
-fn union(mut x: usize, mut y: usize, zs: &mut [Option<usize>], ns: &mut [usize]) {
-    if ns[x] > ns[y] {
+fn union(x: usize, y: usize, zs: &mut [Option<usize>], ns: &mut [usize]) {
+    let mut x = root(x, zs);
+    let mut y = root(y, zs);
+
+    if x == y {
+        return;
+    } else if ns[x] > ns[y] {
         swap(&mut x, &mut y);
     }
 
-    let x = root(x, zs);
-    let y = root(y, zs);
-
-    if x != y {
-        zs[x] = Some(y);
-        ns[y] += ns[x];
-    }
+    zs[x] = Some(y);
+    ns[y] += ns[x];
 }
 
 fn root(mut x: usize, ys: &[Option<usize>]) -> usize {
