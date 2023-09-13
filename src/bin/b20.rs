259df94b0d24004a1a1a14ef4b4dbc8a22ae037a
Yota Toyama <raviqqe@gmail.com>
2023-09-13 23:03:45 +1000

    Fix

--- src/bin/b20.rs
+++ src/bin/b20.rs
@@ -1,3 +1,4 @@
+use algorithms::utility::print_table;
 use proconio::input;
 
 fn main() {
@@ -6,20 +7,54 @@ fn main() {
         t: String,
     }
 
-    let s = s.as_bytes();
-    let t = t.as_bytes();
+    println!("{}", distance(s.as_bytes(), t.as_bytes()));
+}
 
-    let mut dp = vec![vec![0; t.len() + 1]; s.len() + 1];
+fn distance(s: &[u8], t: &[u8]) -> usize {
+    let mut dp = vec![vec![None; t.len() + 1]; s.len() + 1];
+    dp[0][0] = Some(0);
 
     for i in 1..=s.len() {
-        for j in 1..=t.len() {
-            dp[i][j] = if s[i - 1] == t[j - 1] {
-                dp[i - 1][j - 1] + 1
+        for j in 0..=t.len() {
+            if i > 0 && j > 0 && s[i - 1] == t[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1];
             } else {
-                dp[i][j - 1].max(dp[i - 1][j])
-            };
+                dp[i][j] = match (
+                    if i > 0 { dp[i - 1][j] } else { None },
+                    if j > 0 { dp[i][j - 1] } else { None },
+                ) {
+                    (Some(d1), Some(d2)) => Some(d1.min(d2) + 1),
+                    (Some(d), None) | (None, Some(d)) => Some(d + 1),
+                    (None, None) => None,
+                };
+            }
         }
     }
 
-    println!("{}", dp[s.len()][t.len()]);
+    print_table(&dp);
+
+    dp[s.len()][t.len()].unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple() {
+        assert_eq!(distance(b"", b""), 0);
+        assert_eq!(distance(b"a", b"a"), 0);
+        assert_eq!(distance(b"ab", b"ab"), 0);
+        assert_eq!(distance(b"abc", b"abc"), 0);
+    }
+
+    #[test]
+    fn delete() {
+        assert_eq!(distance(b"a", b""), 1);
+        assert_eq!(distance(b"ab", b""), 2);
+        assert_eq!(distance(b"ab", b"a"), 1);
+        assert_eq!(distance(b"ab", b"b"), 1);
+        assert_eq!(distance(b"ab", b""), 2);
+    }
 }
