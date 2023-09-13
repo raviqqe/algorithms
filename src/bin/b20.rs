93e5ecb0988baa67a87d693621709b23e2c88e43
Yota Toyama <raviqqe@gmail.com>
2023-09-13 23:09:49 +1000

    Fix

--- src/bin/b20.rs
+++ src/bin/b20.rs
@@ -14,8 +14,12 @@ fn distance(s: &[u8], t: &[u8]) -> usize {
     let mut dp = vec![vec![None; t.len() + 1]; s.len() + 1];
     dp[0][0] = Some(0);
 
-    for i in 1..=s.len() {
+    for i in 0..=s.len() {
         for j in 0..=t.len() {
+            if i == 0 && j == 0 {
+                continue;
+            }
+
             if i > 0 && j > 0 && s[i - 1] == t[j - 1] {
                 dp[i][j] = dp[i - 1][j - 1];
             } else {
@@ -25,7 +29,10 @@ fn distance(s: &[u8], t: &[u8]) -> usize {
                 ) {
                     (Some(d1), Some(d2)) => Some(d1.min(d2) + 1),
                     (Some(d), None) | (None, Some(d)) => Some(d + 1),
-                    (None, None) => None,
+                    (None, None) => {
+                        dbg!(i, j);
+                        None
+                    }
                 };
             }
         }
@@ -56,5 +63,16 @@ mod tests {
         assert_eq!(distance(b"ab", b"a"), 1);
         assert_eq!(distance(b"ab", b"b"), 1);
         assert_eq!(distance(b"ab", b""), 2);
+        assert_eq!(distance(b"abc", b"ac"), 1);
+    }
+
+    #[test]
+    fn insert() {
+        assert_eq!(distance(b"", b"a"), 1);
+        assert_eq!(distance(b"", b"ab"), 2);
+        assert_eq!(distance(b"", b"abc"), 3);
+        assert_eq!(distance(b"a", b"ab"), 1);
+        assert_eq!(distance(b"b", b"ab"), 1);
+        assert_eq!(distance(b"ac", b"abc"), 1);
     }
 }
