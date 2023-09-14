aaafb1c5e41734ab0ae897baf162883470baaa11
Yota Toyama <raviqqe@gmail.com>
2023-09-14 17:27:14 +1000

    Fix

---
+++ src/bin/b4.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! { s: String }
+
+    let mut x = 0;
+
+    for c in s.chars().rev() {
+        x *= 2;
+        x += if c == '0' { 0 } else { 1 };
+    }
+
+    println!("{}", x);
+}
