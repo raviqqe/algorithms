ec85a19a55240a49da5dcbda77ad8233e9fa4761
Yota Toyama <raviqqe@gmail.com>
2023-09-14 12:25:59 +0930

    B21 (#25)
    - Fix
    - Fix
    - Fix

---
+++ src/bin/b21.rs
@@ -0,0 +1,68 @@
+use proconio::{input, marker::Bytes};
+
+fn main() {
+    input! {
+        _: usize,
+        s: Bytes,
+    }
+
+    println!("{}", solve(&s));
+}
+
+fn solve(s: &[u8]) -> usize {
+    let mut dp = vec![vec![0; s.len()]; s.len()];
+
+    for i in (0..s.len()).rev() {
+        for j in i..s.len() {
+            dp[i][j] = if i == j {
+                1
+            } else {
+                [
+                    dp[i + 1][j - 1] + if s[i] == s[j] { 2 } else { 0 },
+                    dp[i + 1][j],
+                    dp[i][j - 1],
+                ]
+                .into_iter()
+                .max()
+                .unwrap()
+            };
+        }
+    }
+
+    dp.into_iter().flatten().max().unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    // spell-checker: disable
+
+    #[test]
+    fn simple() {
+        assert_eq!(solve(b""), 0);
+        assert_eq!(solve(b"a"), 1);
+        assert_eq!(solve(b"aa"), 2);
+        assert_eq!(solve(b"aaa"), 3);
+    }
+
+    #[test]
+    fn complex() {
+        assert_eq!(solve(b"ab"), 1);
+        assert_eq!(solve(b"aba"), 3);
+        assert_eq!(solve(b"abcde"), 1);
+        assert_eq!(solve(b"abcbe"), 3);
+        assert_eq!(solve(b"abcdcba"), 7);
+        assert_eq!(solve(b"programming"), 4);
+        assert_eq!(solve(b"axcyexg"), 3);
+        assert_eq!(solve(b"axcyyfxh"), 4);
+        assert_eq!(solve(b"axcyeygxi"), 5);
+    }
+
+    #[test]
+    fn asymmetric() {
+        assert_eq!(solve(b"axbcxd"), 3);
+        assert_eq!(solve(b"axyybxc"), 4);
+    }
+}
