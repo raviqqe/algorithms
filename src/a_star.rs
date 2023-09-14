386815a81c63e302d5631504130a2b74b4c9b7c5
Yota Toyama <raviqqe@gmail.com>
2023-09-14 14:33:29 +1000

    Fix

--- src/a_star.rs
+++ src/a_star.rs
@@ -3,7 +3,7 @@ use std::collections::{BinaryHeap, HashMap, HashSet};
 fn search(
     start: usize,
     end: usize,
-    nodes: &[HashSet<usize>],
+    nodes: &[HashSet<(usize, usize)>],
     h: impl Fn(usize) -> usize,
 ) -> Option<usize> {
     let mut q = BinaryHeap::from_iter([start]);
@@ -14,20 +14,21 @@ fn search(
 
     while let Some(i) = q.pop() {
         if i == end {
-            return Some(f[end]);
+            return Some(f[&end]);
         }
 
-        for each neighbor of current
+        for (j, w) in nodes[i] {
             // d(current,neighbor) is the weight of the edge from current to neighbor
             // tentative_gScore is the distance from start to the neighbor through current
-            tentative_gScore := gScore[current] + d(current, neighbor)
-            if tentative_gScore < gScore[neighbor]
-                // This path to neighbor is better than any previous one. Record it!
-                cameFrom[neighbor] := current
-                gScore[neighbor] := tentative_gScore
-                fScore[neighbor] := tentative_gScore + h(neighbor)
-                if neighbor not in openSet
-                    openSet.add(neighbor)
+            // tentative_gScore := gScore[current] + d(current, neighbor)
+            // if tentative_gScore < gScore[neighbor]
+            //     // This path to neighbor is better than any previous one. Record it!
+            //     cameFrom[neighbor] := current
+            //     gScore[neighbor] := tentative_gScore
+            //     fScore[neighbor] := tentative_gScore + h(neighbor)
+            //     if neighbor not in openSet
+            //         openSet.add(neighbor)
+        }
     }
 
     None
