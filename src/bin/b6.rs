ff28ba9fa25df863ddaac96ce22e9b900e6c049f
Yota Toyama <raviqqe@gmail.com>
2023-09-14 18:39:54 +1000

    Fix

---
+++ src/bin/b6.rs
@@ -0,0 +1,37 @@
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        n: usize,
+        xs: [usize; n],
+        m: usize,
+        qs: [(Usize1, Usize1); m],
+    }
+
+    let mut ys = vec![(0, 0); n];
+
+    for i in 0..xs.len() {
+        let x = xs[i];
+        let y = if i > 0 { ys[i - 1] } else { (0, 0) };
+
+        ys[i] = (y.0 + x, y.1 + 1 - x);
+    }
+
+    for (i, j) in qs {
+        let (w0, l0) = if i > 0 { ys[i - 1] } else { (0, 0) };
+        let (w1, l1) = ys[j];
+        let w = w1 - w0;
+        let l = l1 - l0;
+
+        println!(
+            "{}",
+            if w == l {
+                "draw"
+            } else if w < l {
+                "lose"
+            } else {
+                "win"
+            }
+        );
+    }
+}
