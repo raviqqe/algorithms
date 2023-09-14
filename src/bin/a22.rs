79f9deecbd11c5a9cb899acfba4f9fda350f082a
Yota Toyama <raviqqe@gmail.com>
2023-09-14 13:25:53 +1000

    Fix

--- src/bin/a22.rs
+++ src/bin/a22.rs
@@ -15,17 +15,18 @@ fn main() {
 
 fn solve(xs: &[usize], ys: &[usize]) -> usize {
     let n = xs.len() + 1;
-    let mut dp = vec![0; n];
+    let mut dp = vec![None; n];
+    dp[0] = Some(0);
 
     for i in 0..n - 1 {
         for (j, v) in [(xs[i], A), (ys[i], B)] {
-            dp[j] = dp[j].max(dp[i] + v);
+            dp[j] = dp[j].max(dp[i].map(|x| x + v));
         }
     }
 
     dbg!(&dp);
 
-    *dp.last().unwrap()
+    dp.last().unwrap().unwrap()
 }
 
 #[cfg(test)]
@@ -37,6 +38,7 @@ mod tests {
     fn simple() {
         assert_eq!(solve(&[1], &[1]), 150);
         assert_eq!(solve(&[1, 2], &[2, 2]), 250);
+        assert_eq!(solve(&[1, 2], &[1, 2]), 300);
         assert_eq!(solve(&[1, 3, 3, 6, 5, 6], &[2, 4, 5, 6, 6, 6]), 500)
     }
 }
