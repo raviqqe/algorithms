28dcc657f5cb7596fba6651431872064bc0b5d73
Yota Toyama <raviqqe@gmail.com>
2023-09-14 10:55:51 +1000

    Fix

--- src/bin/b21.rs
+++ src/bin/b21.rs
@@ -6,5 +6,40 @@ fn main() {
         s: Bytes,
     }
 
-    dbg!(s);
+    println!("{}", solve(&s));
+}
+
+fn solve(s: &[u8]) -> usize {
+    let mut dp = vec![vec![false; s.len()]; s.len()];
+
+    for i in 0..s.len() {
+        for j in i..s.len() {
+            dp[i][j] = i == j || false;
+        }
+    }
+
+    dp.into_iter()
+        .enumerate()
+        .flat_map(|(i, dp)| {
+            dp.into_iter()
+                .enumerate()
+                .flat_map(move |(j, b)| b.then_some((i as isize - j as isize).unsigned_abs()))
+        })
+        .max()
+        .map(|x| x + 1)
+        .unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple() {
+        assert_eq!(solve(b""), 0);
+        assert_eq!(solve(b"a"), 1);
+        assert_eq!(solve(b"aa"), 2);
+        assert_eq!(solve(b"aaa"), 3);
+    }
 }
