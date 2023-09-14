1656f6cd9059a28a0a209460fbcc715adb07a9a0
Yota Toyama <raviqqe@gmail.com>
2023-09-14 19:11:58 +1000

    Fix

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -1,5 +1,27 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
-    input! { d: usize, n: usize, qs: [(Usize1, Usize1); n] }
+    input! {
+        d: usize,
+        n: usize,
+        qs: [(Usize1, Usize1); n],
+    }
+
+    let mut xs = vec![0; d];
+    let mut ys = vec![0; d];
+
+    for (i, j) in qs {
+        xs[i] += 1;
+        ys[j] += 1;
+    }
+
+    let mut zs = vec![0; d];
+
+    for i in 0..d {
+        zs[i] = (if i == 0 { 0 } else { zs[i - 1] }) + xs[i] - (if i == 0 { 0 } else { ys[i - 1] });
+    }
+
+    for z in zs {
+        println!("{}", z);
+    }
 }
