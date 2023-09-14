fedc78bd7ef6a15235d9414895d31f1497d7c6e8
Yota Toyama <raviqqe@gmail.com>
2023-09-14 16:36:35 +1000

    Fix

---
+++ src/bin/b3_infinite.rs
@@ -0,0 +1,29 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        xs: [(usize, usize); n],
+    }
+
+    println!("{:?}", solve(&xs, m));
+}
+
+fn solve(xs: &[(usize, usize)], m: usize) -> usize {
+    let mut dp = vec![vec![0; m + 1]; xs.len() + 1];
+
+    for i in 0..xs.len() {}
+
+    dp[0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        assert_eq!(solve(&[(2, 5)], 2), 5);
+    }
+}
