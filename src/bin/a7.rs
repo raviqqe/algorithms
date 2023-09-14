3ed159fa5eb867706c42143845b0081a5402b33b
Yota Toyama <raviqqe@gmail.com>
2023-09-14 20:52:32 +0930

    A7 (#33)

---
+++ src/bin/a7.rs
@@ -0,0 +1,24 @@
+use proconio::{input, marker::Usize1};
+
+fn main() {
+    input! {
+        d: usize,
+        n: usize,
+        qs: [(Usize1, Usize1); n],
+    }
+
+    let mut xs = vec![0isize; d + 1];
+
+    for (i, j) in qs {
+        xs[i] += 1;
+        xs[j + 1] -= 1;
+    }
+
+    let mut y = 0;
+
+    for x in &xs[..d] {
+        y += x;
+
+        println!("{}", y);
+    }
+}
---
+++ src/bin/b7.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+fn main() {
+    input! { t: usize, n: usize, xs: [(usize, usize); n] }
+
+    let mut ys = vec![0isize; t + 1];
+
+    for (i, j) in xs {
+        ys[i] += 1;
+        ys[j] -= 1;
+    }
+
+    let mut z = 0;
+
+    for y in &ys[..t] {
+        z += y;
+
+        println!("{}", z);
+    }
+}
