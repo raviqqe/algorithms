2507bc4d1e41e1ff4023f78bdadc1d2e6e3dab56
Yota Toyama <raviqqe@gmail.com>
2023-09-14 14:41:39 +1000

    Fix

--- src/a_star.rs
+++ src/a_star.rs
@@ -1,35 +1,49 @@
 use std::collections::{BinaryHeap, HashMap, HashSet};
 
-fn search(
+// https://en.wikipedia.org/wiki/A*_search_algorithm
+pub fn search(
     start: usize,
     end: usize,
     nodes: &[HashSet<(usize, usize)>],
     h: impl Fn(usize) -> usize,
 ) -> Option<usize> {
     let mut q = BinaryHeap::from_iter([start]);
-    let mut closed = HashMap::new();
+    // let mut closed = HashMap::new();
 
-    let mut f = HashMap::from_iter([(start, h(start))]);
-    let mut g = HashMap::from_iter([(start, 0)]);
+    let mut f = HashMap::<usize, _>::from_iter([(start, h(start))]);
+    let mut g = HashMap::<usize, _>::from_iter([(start, 0)]);
 
     while let Some(i) = q.pop() {
         if i == end {
             return Some(f[&end]);
         }
 
-        for (j, w) in nodes[i] {
-            // d(current,neighbor) is the weight of the edge from current to neighbor
-            // tentative_gScore is the distance from start to the neighbor through current
-            // tentative_gScore := gScore[current] + d(current, neighbor)
-            // if tentative_gScore < gScore[neighbor]
-            //     // This path to neighbor is better than any previous one. Record it!
-            //     cameFrom[neighbor] := current
-            //     gScore[neighbor] := tentative_gScore
-            //     fScore[neighbor] := tentative_gScore + h(neighbor)
-            //     if neighbor not in openSet
-            //         openSet.add(neighbor)
+        for &(j, w) in &nodes[i] {
+            let gg = g[&i] + w;
+
+            if gg < g[&j] {
+                //     cameFrom[neighbor] := current
+                g.insert(j, gg);
+                f.insert(j, gg + h(j));
+
+                q.push(j);
+            }
         }
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn simple() {
+        assert_eq!(
+            search(0, 1, &[[(1, 1)].into_iter().collect()], |_| 0),
+            Some(1)
+        );
+    }
 }
