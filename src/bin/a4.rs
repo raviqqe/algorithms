4f4542da983857d745a0c27c61ce4cf91aeb8e97
Yota Toyama <raviqqe@gmail.com>
2023-09-14 17:21:42 +1000

    Fix

---
+++ src/bin/a4.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+fn main() {
+    input! { mut n: usize }
+
+    let mut xs = vec![];
+
+    if n == 0 {
+        xs.push(0);
+    } else {
+        while n != 0 {
+            xs.push(n % 2);
+            n = n / 2;
+        }
+    }
+
+    for x in xs.into_iter().rev() {
+        print!("{}", x);
+    }
+}
