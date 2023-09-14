3ba302dc0ba49e4e32256ba8eb1a3c12c9bf2f22
Yota Toyama <raviqqe@gmail.com>
2023-09-14 14:30:07 +1000

    Fix

--- src/a_star.rs
+++ src/a_star.rs
@@ -1,18 +1,18 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 fn search(
     start: usize,
     end: usize,
-    node_count: usize,
+    nodes: &[HashSet<usize>],
     h: impl Fn(usize) -> usize,
 ) -> Option<usize> {
     let mut open = BinaryHeap::from_iter([start]);
     let mut closed = HashMap::new();
 
-    let mut g = vec![usize::MAX; node_count];
+    let mut g = vec![usize::MAX; nodes.len()];
     g[start] = 0;
 
-    let mut f = vec![usize::MAX, node_count];
+    let mut f = vec![usize::MAX, nodes.len()];
     f[start] = g[start] + h(start);
 
     while let Some(i) = open.pop() {
@@ -20,18 +20,17 @@ fn search(
             return Some(f[end]);
         }
 
-        // openSet.Remove(current)
-        // for each neighbor of current
-        //     // d(current,neighbor) is the weight of the edge from current to neighbor
-        //     // tentative_gScore is the distance from start to the neighbor through current
-        //     tentative_gScore := gScore[current] + d(current, neighbor)
-        //     if tentative_gScore < gScore[neighbor]
-        //         // This path to neighbor is better than any previous one. Record it!
-        //         cameFrom[neighbor] := current
-        //         gScore[neighbor] := tentative_gScore
-        //         fScore[neighbor] := tentative_gScore + h(neighbor)
-        //         if neighbor not in openSet
-        //             openSet.add(neighbor)
+        for each neighbor of current
+            // d(current,neighbor) is the weight of the edge from current to neighbor
+            // tentative_gScore is the distance from start to the neighbor through current
+            tentative_gScore := gScore[current] + d(current, neighbor)
+            if tentative_gScore < gScore[neighbor]
+                // This path to neighbor is better than any previous one. Record it!
+                cameFrom[neighbor] := current
+                gScore[neighbor] := tentative_gScore
+                fScore[neighbor] := tentative_gScore + h(neighbor)
+                if neighbor not in openSet
+                    openSet.add(neighbor)
     }
 
     None
