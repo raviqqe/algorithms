092b37f93c03c02d1aa1927e7783b86f6fc133ca
Yota Toyama <raviqqe@gmail.com>
2023-09-16 11:21:37 +1000

    Fix

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -1,9 +1,12 @@
 pub struct LruCache<K, V> {
     queue: BinaryHeap<(Instant, K)>,
+    map: HashMap<K, V>,
 }
 
 impl<K, V> LruCache<K, V> {
     pub fn new() -> Self {}
 
-    pub fn get(&mut self, key: K) -> Self {}
+    pub fn get(&mut self, key: K) -> Self {
+        self.get()
+    }
 }
