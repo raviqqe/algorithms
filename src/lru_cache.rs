7c0f5b9344908210c6b60596ff8014c0268a7b18
Yota Toyama <raviqqe@gmail.com>
2023-09-16 11:47:26 +1000

    Fix

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -49,11 +49,13 @@ impl<K: Clone + Eq + Hash, V> LruCache<K, V> {
     }
 
     fn truncate_queue(&mut self) {
-        if self.queue.len() < self.capacity {
+        if self.queue.len() <= self.capacity {
             return;
         }
 
-        self.queue.pop_front();
+        if let Some(key) = self.queue.pop_front() {
+            self.map.remove(&key);
+        }
     }
 }
 
@@ -86,6 +88,28 @@ mod tests {
         cache.insert(1, 2045);
 
         assert_eq!(cache.get(&0), Some(&42));
+        assert_eq!(cache.get(&1), Some(&2045));
+    }
+
+    #[test]
+    fn push_away_key_out_of_one() {
+        let mut cache = LruCache::new(1);
+
+        cache.insert(0, 42);
+        cache.insert(1, 2045);
+
+        assert_eq!(cache.get(&0), None);
+        assert_eq!(cache.get(&1), Some(&2045));
+    }
+
+    #[test]
+    fn push_away_key_out_of_two() {
+        let mut cache = LruCache::new(1);
+
+        cache.insert(0, 42);
+        cache.insert(1, 2045);
+
+        assert_eq!(cache.get(&0), None);
         assert_eq!(cache.get(&1), Some(&2045));
     }
 }
