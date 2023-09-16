b980e390f040152dc571a2a9f07946faa8f00531
Yota Toyama <raviqqe@gmail.com>
2023-09-16 11:22:13 +1000

    Fix

--- src/lru_cache.rs
+++ src/lru_cache.rs
@@ -4,9 +4,16 @@ pub struct LruCache<K, V> {
 }
 
 impl<K, V> LruCache<K, V> {
-    pub fn new() -> Self {}
+    pub fn new() -> Self {
+        Self {
+            queue: Default::default(),
+            map: Default::default(),
+        }
+    }
 
     pub fn get(&mut self, key: K) -> Self {
         self.get()
     }
+
+    fn update_queue(&mut self, key: K) {}
 }
