3a3a1544b422f779616bbcc286fe62f9c28a3903
Yota Toyama <raviqqe@gmail.com>
2023-10-17 19:36:09 +1100

    Fix

--- src/vehicle_routing_problem.rs
+++ src/vehicle_routing_problem.rs
@@ -1,27 +1,30 @@
 pub fn solve(m: usize, xs: &[(f64, f64)]) -> f64 {
     let n = xs.len();
-    let mut dp = vec![vec![f64::INFINITY; n + 1]; 1 << n];
-    dp[0][0] = 0.0;
+    let mut dp = vec![vec![vec![f64::INFINITY; n + 1]; m]; 1 << n];
+    dp[0][0][0] = 0.0;
 
     for i in 0..1 << n {
-        for j in 0..n {
-            if dp[i][j].is_infinite() {
-                continue;
-            }
-
+        for j in 0..m {
             for k in 0..n {
-                if 1 << k & i > 0 {
+                if dp[i][j][k].is_infinite() {
                     continue;
                 }
 
-                let ii = i | 1 << k;
+                for k in 0..n {
+                    if 1 << k & i > 0 {
+                        continue;
+                    }
 
-                dp[ii][k] = dp[ii][k].min(dp[i][j] + distance(j, k, xs));
+                    let ii = i | 1 << k;
+
+                    dp[ii][k] = dp[ii][k].min(dp[i][j] + distance(j, k, xs));
+                }
             }
         }
     }
 
-    dp.last().unwrap()[0]
+    // dp.last().unwrap()[0]
+    todo!()
 }
 
 fn distance(i: usize, j: usize, xs: &[(f64, f64)]) -> f64 {
