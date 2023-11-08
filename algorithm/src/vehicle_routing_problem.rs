e40a69fe23c5e527a88285172cec745ebaac096d
Yota Toyama <raviqqe@gmail.com>
2023-11-08 13:19:57 +1100

    Reconstruct VRP (#43)

--- algorithm/src/vehicle_routing_problem.rs
+++ algorithm/src/vehicle_routing_problem.rs
@@ -4,7 +4,7 @@ use ordered_float::OrderedFloat;
 //
 // Local Search for Vehicle Routing and Scheduling Problems: Review and
 // Conceptual Integration, Funke et al. (2005)
-pub fn solve(m: usize, xs: &[(f64, f64)]) -> f64 {
+pub fn solve(m: usize, xs: &[(f64, f64)]) -> (f64, Vec<Vec<usize>>) {
     let n = xs.len();
     let mut dp = vec![vec![vec![f64::INFINITY; n]; m]; 1 << n];
 
@@ -41,17 +41,66 @@ pub fn solve(m: usize, xs: &[(f64, f64)]) -> f64 {
         }
     }
 
-    *dp.last()
+    let (k, &y) = dp
+        .last()
         .unwrap()
         .last()
         .unwrap()
         .iter()
-        .min_by_key(|&&x| OrderedFloat(x))
-        .unwrap()
+        .enumerate()
+        .min_by_key(|(_, &x)| OrderedFloat(x))
+        .unwrap();
+
+    (y, reconstruct(m, xs, &dp, k, y))
 }
 
 fn distance(i: usize, j: usize, xs: &[(f64, f64)]) -> f64 {
     ((xs[i].0 - xs[j].0).powi(2) + (xs[i].1 - xs[j].1).powi(2)).sqrt()
+}
+
+fn reconstruct(
+    m: usize,
+    xs: &[(f64, f64)],
+    dp: &[Vec<Vec<f64>>],
+    mut k: usize,
+    mut y: f64,
+) -> Vec<Vec<usize>> {
+    let mut zs = vec![vec![]; m];
+    let mut i = dp.len() - 1;
+    let mut j = m - 1;
+
+    while i > 0 {
+        zs[j].push(k);
+
+        i &= !(1 << k);
+
+        (j, k, y, _) = dp[i][j]
+            .iter()
+            .enumerate()
+            .map(move |(kk, &x)| (j, kk, x, y - x - distance(kk, k, xs)))
+            .chain(
+                if j > 0 {
+                    Some(
+                        dp[i][j - 1]
+                            .iter()
+                            .enumerate()
+                            .map(move |(kk, &x)| (j - 1, kk, x, y - x)),
+                    )
+                } else {
+                    None
+                }
+                .into_iter()
+                .flatten(),
+            )
+            .min_by_key(|&(_, _, _, y)| OrderedFloat(y.abs()))
+            .unwrap();
+    }
+
+    for z in &mut zs {
+        z.reverse();
+    }
+
+    zs
 }
 
 #[cfg(test)]
@@ -65,7 +114,7 @@ mod tests {
 
         #[test]
         fn one_stop() {
-            assert_eq!(solve(1, &[(0.0, 0.0)]), 0.0);
+            assert_eq!(solve(1, &[(0.0, 0.0)]).0, 0.0);
         }
 
         #[test]
@@ -73,7 +122,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(1, &stops), 1.0);
+                assert_eq!(solve(1, &stops).0, 1.0);
             }
         }
 
@@ -82,7 +131,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(1, &stops), 2.0);
+                assert_eq!(solve(1, &stops).0, 2.0);
             }
         }
 
@@ -91,7 +140,7 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(1, &stops), 3.0);
+                assert_eq!(solve(1, &stops).0, 3.0);
             }
         }
 
@@ -100,7 +149,7 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(1, &stops), 4.0);
+                assert_eq!(solve(1, &stops).0, 4.0);
             }
         }
     }
@@ -111,7 +160,7 @@ mod tests {
 
         #[test]
         fn one_stop() {
-            assert_eq!(solve(2, &[(0.0, 0.0)]), 0.0);
+            assert_eq!(solve(2, &[(0.0, 0.0)]).0, 0.0);
         }
 
         #[test]
@@ -119,7 +168,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(2, &stops), 0.0);
+                assert_eq!(solve(2, &stops).0, 0.0);
             }
         }
 
@@ -128,7 +177,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(2, &stops), 1.0);
+                assert_eq!(solve(2, &stops).0, 1.0);
             }
         }
 
@@ -137,7 +186,7 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(2, &stops), 2.0);
+                assert_eq!(solve(2, &stops).0, 2.0);
             }
         }
 
@@ -146,7 +195,7 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(2, &stops), 3.0);
+                assert_eq!(solve(2, &stops).0, 3.0);
             }
         }
     }
@@ -157,7 +206,7 @@ mod tests {
 
         #[test]
         fn one_stop() {
-            assert_eq!(solve(3, &[(0.0, 0.0)]), 0.0);
+            assert_eq!(solve(3, &[(0.0, 0.0)]).0, 0.0);
         }
 
         #[test]
@@ -165,7 +214,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(3, &stops), 0.0);
+                assert_eq!(solve(3, &stops).0, 0.0);
             }
         }
 
@@ -174,7 +223,7 @@ mod tests {
             let stops = [(0.0, 0.0), (1.0, 0.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(3, &stops), 0.0);
+                assert_eq!(solve(3, &stops).0, 0.0);
             }
         }
 
@@ -183,7 +232,7 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(3, &stops), 1.0);
+                assert_eq!(solve(3, &stops).0, 1.0);
             }
         }
 
@@ -192,7 +241,98 @@ mod tests {
             let stops = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)];
 
             for stops in stops.into_iter().permutations(stops.len()) {
-                assert_eq!(solve(3, &stops), 2.0);
+                assert_eq!(solve(3, &stops).0, 2.0);
+            }
+        }
+    }
+
+    mod reconstruct {
+        use super::*;
+
+        mod one_vehicle {
+            use super::*;
+            use pretty_assertions::assert_eq;
+
+            #[test]
+            fn one_stop() {
+                assert_eq!(solve(1, &[(0.0, 0.0)]), (0.0, vec![vec![0]]));
+            }
+
+            #[test]
+            fn two_stops() {
+                assert_eq!(solve(1, &[(0.0, 0.0), (1.0, 0.0)]), (1.0, vec![vec![1, 0]]));
+            }
+
+            #[test]
+            fn three_stops() {
+                assert_eq!(
+                    solve(1, &[(0.0, 0.0), (1.0, 0.0), (2.0, 0.0)]),
+                    (2.0, vec![vec![2, 1, 0]])
+                );
+            }
+
+            #[test]
+            fn four_stops() {
+                assert_eq!(
+                    solve(1, &[(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]),
+                    (3.0, vec![vec![2, 3, 1, 0]])
+                );
+            }
+
+            #[test]
+            fn five_stops() {
+                assert_eq!(
+                    solve(
+                        1,
+                        &[(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)]
+                    ),
+                    (4.0, vec![vec![4, 2, 3, 1, 0]])
+                );
+            }
+        }
+
+        mod two_vehicles {
+            use super::*;
+            use pretty_assertions::assert_eq;
+
+            #[test]
+            fn one_stop() {
+                assert_eq!(solve(2, &[(0.0, 0.0)]), (0.0, vec![vec![], vec![0]]));
+            }
+
+            #[test]
+            fn two_stops() {
+                assert_eq!(
+                    solve(2, &[(0.0, 0.0), (1.0, 0.0)]),
+                    (0.0, vec![vec![1], vec![0]])
+                );
+            }
+
+            #[test]
+            fn five_stops() {
+                assert_eq!(
+                    solve(
+                        2,
+                        &[(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)]
+                    ),
+                    (3.0, vec![vec![4], vec![2, 3, 1, 0]])
+                );
+            }
+        }
+
+        mod three_vehicles {
+            use super::*;
+            use pretty_assertions::assert_eq;
+
+            #[test]
+            fn five_stops() {
+                assert_eq!(
+                    solve(
+                        3,
+                        &[(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0), (2.0, 0.0)]
+                    ),
+                    (2.0, vec![vec![4], vec![2], vec![3, 1, 0]])
+                );
             }
         }
     }
