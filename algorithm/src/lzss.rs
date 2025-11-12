585c3311af98b752e62b1f86c2eb73b9c48b2c6f
Yota Toyama <raviqqe@gmail.com>
2025-11-11 16:44:22 -0800

    Fix

--- algorithm/src/lzss.rs
+++ algorithm/src/lzss.rs
@@ -70,7 +70,7 @@ mod tests {
     #[test]
     fn test() {
         let data = b"ABABABABABABABABABABA123123123123";
-        let compressed = compress::<64, 32>(data);
+        let compressed = compress::<64, 256>(data);
 
         assert_eq!(decompress(&compressed), data);
     }
