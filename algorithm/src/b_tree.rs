b382cf4578b9a93df7d8ff52303566f4f8d6ca80
Yota Toyama <raviqqe@gmail.com>
2025-12-13 14:02:52 -0800

    Fix

--- algorithm/src/b_tree.rs
+++ algorithm/src/b_tree.rs
@@ -87,7 +87,7 @@ mod tests {
         const DEGREE: usize = 8;
         let mut tree = BTree::<usize, DEGREE>::new();
 
-        for x in 0..96 {
+        for x in 0..64 {
             assert_eq!(tree.get(&x), None);
 
             tree.insert(x);
