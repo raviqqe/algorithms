fa99ae0e43cadf817c443bb46dd4bc0a0a91a828
Yota Toyama <raviqqe@gmail.com>
2025-12-14 14:01:42 -0800

    Fix

--- algorithm/src/b_tree/node.rs
+++ algorithm/src/b_tree/node.rs
@@ -123,7 +123,7 @@ impl<T: Debug + Ord, const N: usize> Node<T, N> {
     fn remove_left(&mut self) -> T {
         if let Some(node) = self.nodes.get_mut(0) {
             let value = node.remove_left();
-            self.underflow(0, 0);
+            self.underflow(0, false);
             value
         } else {
             self.values.remove(0)
