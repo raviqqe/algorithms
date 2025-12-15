97e79d2554391401bfe17b220aad345495172621
Yota Toyama <raviqqe@gmail.com>
2025-12-14 19:11:00 -0800

    Fix

--- algorithm/src/b_tree/node.rs
+++ algorithm/src/b_tree/node.rs
@@ -167,7 +167,7 @@ impl<T: Debug + Ord, const N: usize> Node<T, N> {
 
             for node in &self.nodes {
                 debug_assert_eq!(node.validate(), depth);
-                debug_assert!(!self.values.is_empty());
+                debug_assert!(!node.values.is_empty());
             }
 
             for ((left, value), right) in self
